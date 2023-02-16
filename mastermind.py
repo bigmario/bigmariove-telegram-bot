6e14e5ec908035ed852f1c2e3033ea0bab1a0143
Mario Castro <mariocastro.pva@gmail.com>
2023-02-16 13:46:57 -0400

    fix

--- mastermind.py
+++ mastermind.py
@@ -90,6 +90,5 @@ updater.start_webhook(
     listen="0.0.0.0",
     port=int(os.environ.get("PORT", 5001)),
     url_path=telegram_bot_token,
+    webhook_url=os.environ.get("URL") + telegram_bot_token,
 )
-updater.bot.setWebhook(os.environ.get("URL") + telegram_bot_token)
-updater.idle()
