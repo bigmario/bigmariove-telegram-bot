a26596d83c77c5f04a37ec6ebf5ba1fd6f002e7b
Mario Castro <mariocastro.pva@gmail.com>
2023-02-16 10:40:16 -0400

    fix token

--- app/credentials.py
+++ app/credentials.py
@@ -2,6 +2,6 @@ from .config import Config
 
 global_settings = Config()
 
-bot_token = "AAHmm2muZ44PHfeuVmJQO4bJKYA4ic7Q5"  # "place your token here"
+bot_token = "5959866203:AAHmm2muZ44PHfeuVmJQO4bJKYA4ic7Q5-c"  # "place your token here"
 bot_user_name = "bigmariove_bot"  # "bot username"
 URL = "https://bigmariove.pythonanywhere.com/"  # "the heroku app link that we will create later"
