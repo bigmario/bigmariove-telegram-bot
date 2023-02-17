da657cfa8087079b5a010be892264eaa6752c590
Mario Castro <mariocastro.pva@gmail.com>
2023-02-17 08:20:50 -0400

    fix

--- mastermind.py
+++ mastermind.py
@@ -26,12 +26,12 @@ def get_word_info(update, context):
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=update.message.text,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
+        prompt="Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: What time is it?\nMarv:",
+        temperature=0.5,
+        max_tokens=60,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
     )
 
     update.message.reply_text(response)
