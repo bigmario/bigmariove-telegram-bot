88ea1bfc76fd175041a19b95610298381bdab7e4
Mario Castro <mariocastro.pva@gmail.com>
2023-02-18 13:14:20 -0400

    fixing max tokens

--- app/mastermind.py
+++ app/mastermind.py
@@ -19,7 +19,7 @@ def __create_generation():
         model="text-davinci-003",
         prompt=prompt + start_sequence,
         temperature=0.9,
-        max_tokens=4000,
+        max_tokens=3000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
